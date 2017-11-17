2378a701756ec558fbdbbd0b6172abed4b0e4b2f
Tobias Tunedal <Tunedal.Tobias@hotmail.com>
2017-11-17 01:27:51 +0100

    Skapat databas med tabeller

--- Offertguiden/Models/Blog.cs
+++ Offertguiden/Models/Blog.cs
@@ -8,16 +8,6 @@ using System.Threading.Tasks;
 
 namespace Offertguiden.Models
 {
-    public class BloggingContext : DbContext
-    {
-        public BloggingContext(DbContextOptions<BloggingContext> options)
-            : base(options)
-        {
-
-        }
-        public DbSet<Blog> Blogs { get; set; }
-
-    }
         public class Blog
         {
             [Key]
--- Offertguiden/Startup.cs
+++ Offertguiden/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Offertguiden.Models;
 
 namespace Offertguiden
 {
@@ -22,6 +24,7 @@ namespace Offertguiden
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<OffertguidenContext>(options => options.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Offertguiden; Integrated Security = True; Connect Timeout = 30;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
